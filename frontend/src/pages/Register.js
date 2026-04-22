2f158cfa4f4967285913c69018d40b834f5ef0b3
Kishan Kumar <252724258+kishankr101@users.noreply.github.com>
2026-04-23 01:03:34 +0530

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,1 +1,28 @@
+import { useState } from "react";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("Registered Successfully");
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <input placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={(e)=>setPassword(e.target.value)} />
+      <button onClick={registerUser}>Register</button>
+    </div>
+  );
+}
+
+export default Register;
