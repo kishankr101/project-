dec56dea393f62ffb65582733148616eed501281
Kishan Kumar <252724258+kishankr101@users.noreply.github.com>
2026-04-23 01:06:26 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+import AddProperty from "./pages/AddProperty";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/add-property" element={<AddProperty />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
