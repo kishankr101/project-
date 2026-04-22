e7970d36f26812bb07ae5881ae592c1be7e717ab
Kishan Kumar <252724258+kishankr101@users.noreply.github.com>
2026-04-23 01:02:55 +0530

    Update login.js

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,1 +1,28 @@
+import { useState } from "react";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginUser = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      window.location.href = "/profile";
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={(e)=>setPassword(e.target.value)} />
+      <button onClick={loginUser}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
